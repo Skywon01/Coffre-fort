cb91e167be1a8ee263cc411471c18445a3c7e08a
Yann <doublet.yann@orange.fr>
2024-04-19 14:26:46 +0200

    upload input style wip

--- front/gupi_angular/src/app/components/employee-list/employee-list.component.ts
+++ front/gupi_angular/src/app/components/employee-list/employee-list.component.ts
@@ -3,6 +3,9 @@ import {NzTableComponent, NzTdAddOnComponent, NzThMeasureDirective, NzTrExpandDi
 import {NgForOf} from "@angular/common";
 import {UserModel} from "../../model/user.model";
 import {SingleEmployeeDisplayerComponent} from "../single-employee-displayer/single-employee-displayer.component";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {NzUploadChangeParam, NzUploadComponent} from "ng-zorro-antd/upload";
+import {NzIconDirective} from "ng-zorro-antd/icon";
 
 @Component({
   selector: 'app-employee-list',
@@ -13,14 +16,27 @@ import {SingleEmployeeDisplayerComponent} from "../single-employee-displayer/sin
         NgForOf,
         NzTdAddOnComponent,
         NzTrExpandDirective,
-        SingleEmployeeDisplayerComponent
+        SingleEmployeeDisplayerComponent,
+        NzUploadComponent,
+        NzIconDirective
     ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
     @Input() tuyauDeUsers!: UserModel[]
-
+    constructor(private msg: NzMessageService) {}
+    handleChange({ file, fileList }: NzUploadChangeParam): void {
+        const status = file.status;
+        if (status !== 'uploading') {
+            console.log(file, fileList);
+        }
+        if (status === 'done') {
+            this.msg.success(`${file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            this.msg.error(`${file.name} file upload failed.`);
+        }
+    }
 
 
 }
