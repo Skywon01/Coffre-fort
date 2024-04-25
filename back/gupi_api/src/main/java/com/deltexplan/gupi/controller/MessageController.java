3fd22f9b425172b6e1d0f29e46f0835362738ba5
Skywon <doublet.yann@orange.fr>
2024-04-25 11:52:23 +0200

    Message api wip

--- back/gupi_api/src/main/java/com/deltexplan/gupi/controller/MessageController.java
+++ back/gupi_api/src/main/java/com/deltexplan/gupi/controller/MessageController.java
@@ -3,14 +3,13 @@ package com.deltexplan.gupi.controller;
 import com.deltexplan.gupi.entity.Message;
 import com.deltexplan.gupi.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/messages")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class MessageController {
     @Autowired
     private MessageService messageService;
--- back/gupi_api/src/main/java/com/deltexplan/gupi/entity/Message.java
+++ back/gupi_api/src/main/java/com/deltexplan/gupi/entity/Message.java
@@ -14,7 +14,16 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Integer id;
+
     private String content;
-    private String sender;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id")
+    private User recipient;
+
     private LocalDateTime timestamp;
 }
