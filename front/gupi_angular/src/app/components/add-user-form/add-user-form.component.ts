04c65a11925077b54e4550ad40297e588e61575b
Yann <doublet.yann@orange.fr>
2024-04-30 09:15:07 +0200

    clean form

--- front/gupi_angular/src/app/components/add-user-form/add-user-form.component.ts
+++ front/gupi_angular/src/app/components/add-user-form/add-user-form.component.ts
@@ -1,26 +1,21 @@
 import {Component} from '@angular/core';
 import {
-    AbstractControl,
     FormControl,
     FormGroup,
-    NonNullableFormBuilder,
     ReactiveFormsModule,
-    ValidatorFn,
     Validators
 } from "@angular/forms";
 import {
     NzFormControlComponent,
     NzFormDirective,
     NzFormItemComponent,
-    NzFormLabelComponent,
-    NzFormTooltipIcon
+    NzFormLabelComponent
 } from "ng-zorro-antd/form";
 import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
 import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
 import {NzOptionComponent, NzSelectComponent} from "ng-zorro-antd/select";
 import {NzButtonComponent} from "ng-zorro-antd/button";
 import {NzCheckboxComponent} from "ng-zorro-antd/checkbox";
-import {of} from "rxjs";
 import {NgForOf} from "@angular/common";
 import {UserModel} from "../../model/user.model";
 import {ApiService} from "../../services/api.service";
@@ -75,50 +70,6 @@ export class AddUserFormComponent {
                     control.updateValueAndValidity({onlySelf: true});
                 }
             });
-
-
-            // validateForm: FormGroup<{
-            //     user_id: FormControl<string>;
-            //     name: FormControl<string>;
-            //     firstName: FormControl<string>;
-            //     email: FormControl<string>;
-            //     age: FormControl<string>;
-            //     address: FormControl<string>;
-            //
-            //
-            //
-            // }>;
-            //
-            //
-            // submitForm() {
-            //     const formData: UserModel = this.validateForm.value;
-            //     console.log('Données du formulaire à envoyer :', formData);
-            //     this.apiService.registerUser(formData).subscribe()
-            //     // if (this.validateForm.valid) {
-            //     //     console.log('submit', this.validateForm.value);
-            //     // } else {
-            //     //     Object.values(this.validateForm.controls).forEach(control => {
-            //     //         if (control.invalid) {
-            //     //             control.markAsDirty();
-            //     //             control.updateValueAndValidity({onlySelf: true});
-            //     //         }
-            //     //     });
-            //     // }
-            // }
-            //
-            //
-            // constructor(private fb: NonNullableFormBuilder) {
-            //     this.validateForm = this.fb.group({
-            //         user_id: [''],
-            //         name: ['', [Validators.required]],
-            //         firstName: ['', [Validators.required]],
-            //         email: ['', [Validators.email, Validators.required]],
-            //         age: ['', Validators.min(18)],
-            //         address: ['', [Validators.required]],
-            //
-            //     });
-            //     this.numbers = Array.from({length: 99}, (_, i) => i + 1);
-
 
         }
     }
