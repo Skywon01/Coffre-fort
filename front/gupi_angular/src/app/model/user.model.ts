da4971750b8fd25b371deb91224342393e714b29
Yann <doublet.yann@orange.fr>
2024-07-23 15:16:49 +0200

    profile image update

--- front/gupi_angular/src/app/model/user.model.ts
+++ front/gupi_angular/src/app/model/user.model.ts
@@ -7,6 +7,7 @@ export class UserModel {
         public address: string,
         public age: number,
         public password: string,
+        public profile: string,
         public directory: number,
         public file: number,
         public role: string
--- front/gupi_angular/src/app/services/user.service.ts
+++ front/gupi_angular/src/app/services/user.service.ts
@@ -28,7 +28,7 @@ export class UserService {
         const temp: UserModel[] = []
 
         rawdata.map((el) => {
-            let tempObj: UserModel = new UserModel(el.id, el.name, el.firstName, el.email, el.address, el.age, el.password, el.directory, el.file, el.role)
+            let tempObj: UserModel = new UserModel(el.id, el.name, el.firstName, el.email, el.address, el.age, el.password, el.profile, el.directory, el.file, el.role)
             temp.push(tempObj);
         });
         // console.log('Data user: ', temp)
