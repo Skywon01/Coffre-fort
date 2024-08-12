18346694f498dd00a5b4d3042454e6e417536852
Skywon <doublet.yann@orange.fr>
2024-08-12 10:24:31 +0200

    Fix test

--- front/gupi_angular/src/app/services/authentification/auth.service.spec.ts
+++ front/gupi_angular/src/app/services/authentification/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
 import { UserModel } from '../../model/user.model';
+import {apiRoot, httpoptions} from "../api.service";
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -25,13 +26,13 @@ describe('AuthService', () => {
   });
 
   it('devrait récupérer le profil utilisateur et mettre à jour la session', () => {
-    const mockUser: UserModel = { 
-        id: 1, 
-        name: 'Test User', 
-        email: 'test@example.com', 
+    const mockUser: UserModel = {
+        id: 1,
+        name: 'Test User',
+        email: 'test@example.com',
         role: 'USER',
         firstName: '',
-       
+
       } as UserModel;
     spyOn(sessionStorage, 'getItem').and.returnValue(JSON.stringify({ id: 1 }));
     spyOn(sessionStorage, 'setItem');
@@ -42,7 +43,7 @@ describe('AuthService', () => {
       expect(sessionStorage.setItem).toHaveBeenCalledWith('roles', JSON.stringify(mockUser.role));
     });
 
-    const req = httpMock.expectOne(`${service['apiUrl']}/users/1`);
+    const req = httpMock.expectOne(`${service}/users/1`);
     expect(req.request.method).toBe('GET');
     req.flush(mockUser);
   });
